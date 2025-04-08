95b7f0ce522fe4223da5c7c64b2e54e2995e541b
UploadSoulzhgrain <uploadsoul@outlook.com>
2025-04-07 23:58:30 -0700

    feat: update sitemap implementation and fix routing issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
 import './index.css'
 import './i18n/i18n.js' // Import i18n configuration
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </StrictMode>,
 )
